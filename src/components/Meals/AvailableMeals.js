036fa6a540ff862f3bf449c3d618422548a6d566
JiongnanLiu <jia7833@hotmail.com>
2021-10-10 00:45:52 -0400

    add MealItem and MealItemForm and universal Input components

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from "react";
 import classes from './AvailableMeals.module.css';
+import MealItem from "./MealItem/MealItem";
+import Card from "../UI/Card";
 
 const DUMMY_MEALS = [
     {
@@ -25,17 +27,25 @@ const DUMMY_MEALS = [
         name: 'Green Bowl',
         description: 'Healthy...and green...',
         price: 18.99,
-    },
-];
+    },];
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealList = DUMMY_MEALS.map(meal =>
+        <MealItem
+            id={meal.id}
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />);
 
     return (
         <section className={classes.meals}>
-            <ul>
-                {mealList}
-            </ul>
+            <Card>
+                <ul>
+                    {mealList}
+                </ul>
+            </Card>
         </section>
     )
 }
